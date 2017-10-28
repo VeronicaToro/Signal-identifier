e4bbf192d6774f14b47be5eb54fe87a464bcaea2
VeronicaToro <vetorobe@gmail.com>
2017-10-28 11:20:33 -0500

    Add files via upload

---
+++ Algoritmo_final.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  9 09:42:18 2017
+
+@author: veronica
+"""
+import numpy as np
+from Parametros_Final import main1
+from sklearn.externals import joblib
+from sklearn.preprocessing import StandardScaler
+
+#Adjuntos están los dos archivos que aquí se nombran
+
+#fy = np.load('BPSK_Dataset_1024_500datos_minus20to18SNR_25datoscu.npy')
+fy = np.load('BFSK_Dataset_1024_500datos_minus20to18SNR_25datoscu.npy')
+
+#Se toma una fila de la matriz contenida en el archivo. En este caso
+#se toma la fila 400. Los datos que ingresan a la clase son los de la FFT de la señal
+
+ffty=abs(np.fft.fft(fy[400,1:]))
+main1(ffty)     #Desde aquí se imprimen los valores de Amplitud, Frecuencia y Ancho de banda
+
+
+mlp=joblib.load('Redes_Lin_3Layers_10Percep_cu.pkl') #Se carga el archivo que contiene el modelo de redes neuronales
+R=mlp.predict(scaler.transform(fy[400,1:]))
+if R==0:
+    print 'Es BPSK'
+else:
+    print 'Es BFSK'
---
+++ Amplitude_Envelope.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  9 09:42:18 2017
+
+@author: veronica
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+#import scipy.io.wavfile
+#from scipy.signal import hilbert
+#import random
+from scipy.interpolate import interp1d
+
+class Amplitude_Envelope():
+
+    def __init__(self, fy, c=20):
+        self.fy = fy
+        self.c = c
+        self.Amp_env_peak=np.zeros(1)
+
+    def Amp_env(self):
+        ae=abs(self.fy)
+        maxs=np.zeros(int(len(ae)/float(self.c))+2)
+#        maxs[0]=2
+        self.Amp_env_peak[0]=ae[0]
+        i=0
+        for j in np.arange(1,int(len(ae)),self.c):
+            maxs[i]=ae[j:j+self.c].argmax()+j
+            fx=interp1d([maxs[i-1],maxs[i]],[ae[maxs[i-1]],ae[maxs[i]]])
+            xa=np.arange(maxs[i-1],maxs[i])
+            ynew=fx(xa)
+            self.Amp_env_peak=np.concatenate([self.Amp_env_peak,ynew])
+            i+=1
+        self.Amp_env_peak=np.delete(self.Amp_env_peak,0)
+#        j+=1
+#        maxs[i]=ae[j:j+len(ae)].argmax()+j
+#        fx=interp1d([maxs[i-1],maxs[i]],[ae[maxs[i-1]],ae[maxs[i]]])
+#        xa=np.arange(maxs[i-1],maxs[i])
+#        ynew=fx(xa)
+#        self.Amp_env_peak=np.concatenate([self.Amp_env_peak,ynew])
+        return self.Amp_env_peak
+
+    def Graphic(self):
+        fig, ax = plt.subplots()
+        ax.plot(abs(self.fy),'red',label=u'Señal original')
+        ax.plot(self.Amp_env_peak,'k--',label=u'Aproximación por picos')
+
+#        np.save('Amp_env.npy',Amp_env_peak[0:int(len(Amp_env_peak)/2.0)])
+
+        plt.title(u'Primera aproximación')
+        plt.ylabel('Amplitud [u.a.]')
+        plt.xlabel(u'Número de datos')
+        plt.legend()
+        plt.show()
+
+    def Graphic_English(self):
+        fig, ax = plt.subplots()
+        ax.plot(abs(self.fy),'red',label=u'Signal')
+        ax.plot(self.Amp_env_peak,'k--',label=u'Peak approximation')
+
+#        np.save('Amp_env.npy',Amp_env_peak[0:int(len(Amp_env_peak)/2.0)])
+
+        plt.title(u'Approximation')
+        plt.ylabel('Amplitude [a.u.]')
+        plt.xlabel(u'Data number')
+        plt.legend()
+        plt.show()
